cd0b53a042c889b2162d6c699cc0c5ce4d524d06
valter correia <baldhuino@gmail.com>
2023-05-31 14:27:48 +0100

    CommandLineRunner interface implemented

--- src/main/java/com/devb/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/devb/simpleapi/SimpleApiApplication.java
@@ -1,13 +1,22 @@
 package com.devb.simpleapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devb.simpleapi.model.Student;
+
 @SpringBootApplication
-public class SimpleApiApplication {
+public class SimpleApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Student student1 = new Student(null,"Balduino","Mendes","baldhuino@gmail.com","933018608");
+		
 	}
 
 }
