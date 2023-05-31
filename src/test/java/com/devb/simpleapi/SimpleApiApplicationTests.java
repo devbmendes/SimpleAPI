83d531cc2539cc8a666f19840bc2bbe193ff06b0
valter correia <baldhuino@gmail.com>
2023-05-31 13:34:04 +0100

    Project Loaded

---
+++ src/test/java/com/devb/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devb.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
