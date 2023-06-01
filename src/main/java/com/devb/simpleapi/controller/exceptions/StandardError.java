4e1a3d834bb0c7fc585a70c87a01a54004f02d4e
valter correia <baldhuino@gmail.com>
2023-06-01 20:42:16 +0100

    StandardError class implemented

---
+++ src/main/java/com/devb/simpleapi/controller/exceptions/StandardError.java
@@ -0,0 +1,38 @@
+package com.devb.simpleapi.controller.exceptions;
+
+public class StandardError {
+	
+	private Long timestamp;
+	private Integer status;
+	private String message;
+	public Long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public StandardError(Long timestamp, Integer status, String message) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.message = message;
+	}
+	public StandardError() {
+		
+	}
+	
+	
+
+}
