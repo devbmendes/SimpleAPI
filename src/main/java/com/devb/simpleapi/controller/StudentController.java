46d843e5b3730c13d237632319548816dd84bdaf
valter correia <baldhuino@gmail.com>
2023-06-02 17:12:27 +0100

    endpoint changed

--- src/main/java/com/devb/simpleapi/controller/StudentController.java
+++ src/main/java/com/devb/simpleapi/controller/StudentController.java
@@ -22,7 +22,7 @@ import com.devb.simpleapi.service.StudentService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/student")
+@RequestMapping("/v1/student")
 @CrossOrigin("**")
 public class StudentController {
 
